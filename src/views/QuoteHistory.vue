7a8548e5c36d0d5e2d62c207bd2f3ee86479351a
Aian <95435147+4thDimensionDuck@users.noreply.github.com>
2024-03-07 20:50:23 +0800

    Finished Quote History initial layouting

--- src/views/QuoteHistory.vue
+++ src/views/QuoteHistory.vue
@@ -13,6 +13,65 @@
                     <div class="right">
                          <span class="date">01/01/24</span>
                     </div>
+                    <div class="center">
+                         <div class="top"><span class="start">Client</span><span class="end">Total</span></div>
+                         <div class="mid"><span class="start">Company</span><span class="end">Sales Tax</span></div>
+                         <div class="bot"><span class="start">Sales Person</span><span class="end">Register ID</span></div>
+                    </div>
+                    <div class="left">
+                         <span class="link edit">Edit</span>
+                         <span class="link export">Export as PDF</span>
+                         <span class="link print">Print</span>
+                         <spand class="link delete">Delete</spand>
+                    </div>
+               </div>
+               <div class="item">
+                    <div class="right">
+                         <span class="date">01/01/24</span>
+                    </div>
+                    <div class="center">
+                         <div class="top"><span class="start">Client</span><span class="end">Total</span></div>
+                         <div class="mid"><span class="start">Company</span><span class="end">Sales Tax</span></div>
+                         <div class="bot"><span class="start">Sales Person</span><span class="end">Register ID</span></div>
+                    </div>
+                    <div class="left">
+                         <span class="link edit">Edit</span>
+                         <span class="link export">Export as PDF</span>
+                         <span class="link print">Print</span>
+                         <spand class="link delete">Delete</spand>
+                    </div>
+               </div>
+               <div class="item">
+                    <div class="right">
+                         <span class="date">01/01/24</span>
+                    </div>
+                    <div class="center">
+                         <div class="top"><span class="start">Client</span><span class="end">Total</span></div>
+                         <div class="mid"><span class="start">Company</span><span class="end">Sales Tax</span></div>
+                         <div class="bot"><span class="start">Sales Person</span><span class="end">Register ID</span></div>
+                    </div>
+                    <div class="left">
+                         <span class="link edit">Edit</span>
+                         <span class="link export">Export as PDF</span>
+                         <span class="link print">Print</span>
+                         <spand class="link delete">Delete</spand>
+                    </div>
+               </div>
+               <div class="item">
+                    <div class="right">
+                         <span class="date">01/01/24</span>
+                    </div>
+                    <div class="center">
+                         <div class="top"><span class="start">Client</span><span class="end">Total</span></div>
+                         <div class="mid"><span class="start">Company</span><span class="end">Sales Tax</span></div>
+                         <div class="bot"><span class="start">Sales Person</span><span class="end">Register ID</span></div>
+                    </div>
+                    <div class="left">
+                         <span class="link edit">Edit</span>
+                         <span class="link export">Export as PDF</span>
+                         <span class="link print">Print</span>
+                         <spand class="link delete">Delete</spand>
+                    </div>
                </div>
           </div>
      </div>
@@ -62,10 +121,10 @@
           display: flex;
           flex-direction: column;
           background-color: #D9D9D9;
-          height: 75vh;
+          min-height: 75vh;
           margin-left: auto;
           margin-right: auto;
-          margin-bottom: auto;
+          margin-bottom: .5em;
           width: 90%;
           padding: 1.5em;
           border-radius: 25px;
@@ -78,6 +137,11 @@
           border-radius: 25px;
           background-color: #E9E9E9;
           background-clip: border-box;
+          margin-bottom: 2em;
+     }
+
+     .item:last-child {
+          margin-bottom: 0;
      }
 
      .item .right {
@@ -98,9 +162,44 @@
      }
 
      .item .center {
-
+          display: flex;
+          width: 75%;
+          height: inherit;
+          padding: 0 2em;
+          margin: 1em 0;
+          border-right: 2px solid #D9D9D9;
+          justify-content: space-between;
+          flex-direction: column;
      }
-     .item .left {
 
+     .item .center div {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          font-size: large;
+     }
+
+     .item .center .top span{
+          font-size: x-large;
+          font-weight: bold;
+     }
+
+     .item .left {
+          display: flex;
+          flex-direction: column;
+          width: 13%;
+          height: inherit;
+          align-items: center;
+          justify-content: center;
+     }
+
+     .item .left .link {
+          color:#355E3B;
+          font-size: 1.1em;
+          text-decoration: underline;
+          cursor: pointer;
+     }
+     .item .left .link.delete {
+          color:#DA2B2B;
      }
 </style>
