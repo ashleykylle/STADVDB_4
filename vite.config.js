fc70467fe06f2519064754ae8f19da900157bf54
ProfRedacted <king.uy234@gmail.com>
2024-04-05 11:50:08 +0800

    Fixed .env and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,17 +12,22 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  define: {
+    __APP_ENV__: import.meta.env.APP_ENV,
   }
 })
-
-export default defineConfig(({ command, mode }) => {
+/**
+ * export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '')
   return {
     // vite config
     define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV),
+      __APP_ENV__: import.meta.env.VITE_API_URL,
     },
   }
 })
+
+ */
